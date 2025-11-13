5c612d37bbc80cd0a10244b562d6bec559993d2f
Amine <boussaidaminefes@gmail.com>
2025-11-13 04:37:08 +0100

    add method GET

--- proxy.php
+++ proxy.php
@@ -1,7 +1,7 @@
 <?php
 // proxy.php
 
-$apiUrl = 'https://localhost/omeka-s/api'; 
+$apiUrl = 'http://localhost/omeka-s/api';
 $apiKeyIdentity = '98LsLpxqVAHlTqk5ql0gZjazTekis9Br';
 $apiKeyCredential = 'UlCuDAUpaY5UT6p3OF0b1USLNKFuRyZO';
 
@@ -26,9 +26,22 @@ if ($method === 'POST' || $method === 'PUT') {
 
 $response = curl_exec($ch);
 $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+$curl_error = curl_error($ch);
 
 header('Content-Type: application/json');
 http_response_code($httpcode);
-echo $response;
+
+// Debug: Log the response for troubleshooting
+error_log("API Response: " . $response);
+error_log("HTTP Code: " . $httpcode);
+error_log("CURL Error: " . $curl_error);
+
+if ($curl_error) {
+    echo json_encode(['error' => 'CURL Error: ' . $curl_error]);
+} elseif (empty($response) && $httpcode != 200) {
+    echo json_encode(['error' => 'Empty response from API', 'http_code' => $httpcode]);
+} else {
+    echo $response;
+}
 
 curl_close($ch);
